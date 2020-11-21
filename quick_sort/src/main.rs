a23c5a7283a754b69abd073a7f9a3c93837ddb0a
AnthonyK213 <tjjzqwh@163.com>
2020-11-21 14:54:25 +0800

    201121

--- quick_sort/src/main.rs
+++ quick_sort/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let sort_list = vec![3, 1, 4, 1, 5, 9, 2, 6];
-    println!("{:?}", quick_sort(&sort_list));
+    let sort_int: Vec<u32>  = vec![3, 1, 4, 1, 5, 9, 2, 6];
+    let sort_flt: Vec<f32>  = vec![3.14, 1.0, 4.0, 1.0, 5.0, 9.0, 2.0, 6.0];
+    let sort_str: Vec<char> = "The five boxing wizards jump quickly".to_lowercase().chars().collect();
+    println!("{:?}", quick_sort(&sort_int));
+    println!("{:?}", quick_sort(&sort_flt));
+    println!("{:?}", quick_sort(&sort_str));
 }
 
-fn quick_sort<T: Ord + Copy>(set: &Vec<T>) -> Vec<T> {
+fn quick_sort<T>(set: &Vec<T>) -> Vec<T>
+    where T: PartialOrd + Copy {
     match set.len() {
         0 | 1 => set.to_vec(),
         _ => {
