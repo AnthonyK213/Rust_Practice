8be432e93a79beaf00e0d3fdce7950a66bacc59e
AnthonyK213 <tjjzqwh@163.com>
2020-11-29 16:42:46 +0800

    201129

--- eight_queens/src/main.rs
+++ eight_queens/src/main.rs
@@ -1,18 +1,18 @@
 static mut COUNT: usize = 0;
 
 fn main() {
-    let queen: usize = 8;
+    let queen: usize = 12;
 
     let mut qlist: Vec<i32> = Vec::new();
     for i in 0..queen {
         qlist.push(i as i32);
     }
 
-    n_queens(&mut qlist, 0, queen);
+    n_queens(&mut qlist, 0, &queen);
 }
 
-fn n_queens(result: &mut Vec<i32>, split: usize, queen: usize) {
-    if split == queen {
+fn n_queens(result: &mut Vec<i32>, split: usize, queen: &usize) {
+    if split == *queen {
         unsafe {
             COUNT += 1;
             println!("{:?} {}", result, COUNT);
@@ -20,7 +20,7 @@ fn n_queens(result: &mut Vec<i32>, split: usize, queen: usize) {
         return;
     }
 
-    for i in split..queen {
+    for i in split..*queen {
         let mut res_cp = result.to_vec();
         let mut is_ok = true;
 
@@ -33,7 +33,7 @@ fn n_queens(result: &mut Vec<i32>, split: usize, queen: usize) {
 
         if is_ok {
             res_cp.swap(split, i);
-            n_queens(&mut res_cp, split + 1, queen);
+            n_queens(&mut res_cp, split + 1, &queen);
         }
     }
 }
