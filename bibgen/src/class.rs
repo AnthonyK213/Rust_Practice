ea21114ea66b950e0d0d18015224f3b758b7d8fb
AnthonyK213 <tjjzqwh@163.com>
2021-03-26 02:07:31 +0800

    rewrite_genref_as_bibgen

---
+++ bibgen/src/class.rs
@@ -0,0 +1,96 @@
+use std::fs;
+use std::path::Path;
+use std::io::{BufRead, BufReader};
+
+pub struct BibNE {
+    content: Vec<String>
+}
+
+impl BibNE {
+    pub fn new(path: &Path) -> Self {
+        let file = fs::File::open(path).unwrap();
+        let reader = BufReader::new(file);
+        let mut content: Vec<String> = Vec::new();
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if line != "" {
+                content.push(line);
+            }
+        }
+        Self { content }
+    }
+
+    fn title(&self) -> String {
+        let mut tt_str = "--".to_string();
+
+        for line in &self.content {
+            let ne_line: Vec<&str> = line.split(": ").collect();
+
+            if line != "" && ne_line.len() > 1 {
+                let key: &str = &ne_line[0];
+                let val: &str = &ne_line[1]
+                    .chars()
+                    .map(|x| match x {
+                        ' ' | '(' | ')' | ',' | '{' | '}' => '_',
+                        _ => x
+                    }).collect::<String>();
+                match key {
+                    "{Title}" => {
+                        tt_str = val.to_string();
+                        break;
+                    },
+                    _ => {}
+                };
+            }
+        }
+        tt_str
+    }
+
+    pub fn gen(&self) -> String {
+        let mut bib_txt: String = "".to_string();
+
+        for line in &self.content {
+            let ne_line: Vec<&str> = line.split("}: ").collect();
+
+            if line != "" && ne_line.len() > 1 {
+                let new_key: &str = &ne_line[0][1..]
+                    .to_lowercase()
+                    .replace(" ", "")
+                    .replace(",", "");
+                let new_val: &str = ne_line[1];
+                let ttl = self.title();
+                match new_key {
+                    "referencetype" => {
+                        match new_val {
+                            "Journal Article"        => bib_txt.push_str(&format!("@article{{{},\n",       &ttl)),
+                            "Conference Proceedings" => bib_txt.push_str(&format!("@inproceedings{{{},\n", &ttl)),
+                            "Thesis"                 => bib_txt.push_str(&format!("@mastersthesis{{{},\n", &ttl)),
+                            _ => {
+                                bib_txt.push_str(&format!("//@{}{{{},\n", new_val, &ttl));
+                                println!("Unknown type of article: \"{}\" => {}", new_val, &ttl);
+                            }
+                        }
+                    },
+                    "issue" => bib_txt.push_str(&format!("  number={{{}}},\n", &new_val)), 
+                    "abstract" => {},
+                    _ => bib_txt.push_str(&format!("  {}={{{}}},\n", &new_key, &new_val))
+                }
+            }
+        }
+        bib_txt + ("}\n\n")
+    }
+}
+
+pub fn bl2st(bl_path: &Path) -> String {
+    let file = fs::File::open(bl_path).unwrap();
+    let reader = BufReader::new(file);
+    let mut content: String = String::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line != "" {
+            content += &line;
+            content += "\n";
+        }
+    }
+    content + "\n"
+}
---
+++ bibgen/src/main.rs
@@ -0,0 +1,33 @@
+mod class;
+use class::*;
+use std::{fs, env};
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let dir_name: &str = &args[1];
+
+    let ne_dir = "./";
+    let dir_path = &format!("{}{}", ne_dir, dir_name);
+
+    let mut bib_list: String = String::from("");
+
+    let paths = fs::read_dir(dir_path).unwrap();
+    for path in paths {
+        let file_path = path.unwrap().path();
+        let file_extn = file_path.extension();
+        if let Some(extn) = file_extn {
+            match extn.to_str().unwrap() {
+                "net" => {
+                    let bib_ne = BibNE::new(&file_path);
+                    bib_list.push_str(&bib_ne.gen());
+                },
+                "bib" => {bib_list.push_str(&bl2st(&file_path));},
+                _ => {}
+            }
+        }
+    }
+
+    // Is this correct?
+    fs::write(format!("{}ref.bib", ne_dir), bib_list).unwrap();
+}
